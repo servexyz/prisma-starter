f440bd73d806f6d3fbed7c4caa4d5742dd09709d
alechp <me@alechp.com>
2018-08-16 16:35:59 -0700

    w0000t fixed the errconn issue. Now, need to figure out $_where error. W00t new errors ! finally

--- src/service/index.js
+++ src/service/index.js
@@ -24,7 +24,7 @@ const server = new GraphQLServer({
     ...req,
     db: new Prisma({
       typeDefs: "service/generated/prisma.graphql", // the auto-generated GraphQL schema of the Prisma API
-      endpoint: PRISMA_ENDPOINT, // the endpoint of the Prisma API
+      endpoint: "http://prisma:4466", // the endpoint of the Prisma API
       debug: true // log all GraphQL queries & mutations sent to the Prisma API
       // secret: process.env.PRISMA_SECRET // only needed if specified in `database/prisma.yml`
     })
