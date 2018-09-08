8950863d5c467db8df56f5b8b1e0e1b9ee44c0ce
alechp <me@alechp.com>
2018-09-08 13:28:26 -0700

    Cleared up es6 tests

--- src/service/tests/es6.js
+++
@@ -1,6 +0,0 @@
-import got from "got";
-import fs from "fs";
-
-export default async function(url, outputFile) {
-  got.stream(url).pipe(fs.createWriteStream(outputFile));
-}
--- src/service/tests/foo.test.js
+++ src/service/tests/foo.test.js
@@ -1,7 +1,5 @@
 import test from "ava";
-import sample from "./es6";
-import path from "path";
-import got from "got";
+
 test("foo", t => {
   t.pass();
 });
@@ -10,17 +8,4 @@ test("bar", async t => {
   const bar = Promise.resolve("bar");
 
   t.is(await bar, "bar");
-});
-
-const url = "https://jsonplaceholder.typicode.com/comments";
-test("import works even though no babel", t => {
-  const filePath = path.join(__dirname, "output.json");
-  sample(url, filePath);
-  t.pass();
-});
-
-test("got works", async t => {
-  let foo = await got(url);
-  console.log(foo);
-  t.pass();
 });
