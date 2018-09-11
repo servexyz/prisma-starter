593c9727446a7a5d5ff1a5baa116566c37ad4b8c
alechp <me@alechp.com>
2018-09-11 15:27:03 -0700

    Local still not working, but curious to see how travis responds to all 4

--- src/service/tests/prisma.test.js
+++ src/service/tests/prisma.test.js
@@ -10,37 +10,22 @@ test.before(async t => {
   console.log(
     "Waiting 3 seconds before restarting to prevent race case with nodemon's watch"
   );
-  await delay(3000);
+  // await delay(3000);
+  isTravis ? await delay(10000) : await delay(5000);
 });
 
-// var endpoint4000 = isTravis
-//   ? `${process.env.PRISMA_HOST}:4000`
-//   : `http://localhost:4000`;
+test(`prisma:4000 is reachable`, async t => {
+  t.true(await isReachable("prisma:4000"));
+});
 
-// var endpoint4466 = isTravis
-//   ? `${process.env.PRISMA_HOST}:4466`
-//   : `http://localhost:4466`;
+test(`prisma:4466 is reachable`, async t => {
+  t.true(await isReachable("prisma:4466"));
+});
 
-test(`http://localhost:4000 is reachable`, async t => {
-  console.log("localhost:4000: ", await isReachable("localhost:4000"));
-  console.log(
-    "http://localhost:4000: ",
-    await isReachable("http://localhost:4000")
-  );
-  console.log(
-    "https://localhost:4000: ",
-    await isReachable("https://localhost:4000")
-  );
-  console.log("prisma:4000: ", await isReachable("prisma:4000"));
-  console.log("http://prisma:4000: ", await isReachable("http://prisma:4000"));
-  console.log(
-    "https://prisma:4000: ",
-    await isReachable("https://prisma:4000")
-  );
-
+test(`localhost:4000 is reachable`, async t => {
   t.true(await isReachable("localhost:4000"));
 });
 
-test(`http://localhost:4466 is reachable`, async t => {
+test(`localhost:4466 is reachable`, async t => {
   t.true(await isReachable("localhost:4466"));
 });
