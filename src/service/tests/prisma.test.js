bf5309be9d6d4d914a455508a439048f9b37da6e
alechp <me@alechp.com>
2018-09-11 11:24:48 -0700

    Started using dynamic endpoint in tests instead of hard-coded localhost

--- src/service/tests/prisma.test.js
+++ src/service/tests/prisma.test.js
@@ -19,10 +19,10 @@ test("process.env.PRISMA_HOST is set", t => {
 test(`${process.env.PRISMA_HOST}:4000 is reachable`, async t => {
   console.log("foo");
   let endpoint = `${process.env.PRISMA_HOST}:4000`;
-  t.true(await isReachable("localhost:4000"));
+  t.true(await isReachable(endpoint));
 });
 
 test(`${process.env.PRISMA_HOST}:4466 is reachable`, async t => {
   let endpoint = `${process.env.PRISMA_HOST}:4466`;
-  t.true(await isReachable("localhost:4000"));
+  t.true(await isReachable(endpoint));
 });
