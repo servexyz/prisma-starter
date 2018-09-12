f15a5443e9c3b1f1e54f5a6850c0dd9f131f5a6d
alechp <me@alechp.com>
2018-09-11 18:39:57 -0700

    Re-enabled is-reachable tests & decreased delay time

--- src/service/tests/prisma.test.js
+++ src/service/tests/prisma.test.js
@@ -5,21 +5,18 @@ const isTravis = require("is-travis");
 
 test.before(async t => {
   //TODO: Figure out less lazy solution; https://github.com/servexyz/prisma-starter/pull/25#issuecomment-419687114
-  let ms = isTravis ? 10000 : 5000;
+  let ms = isTravis ? 8000 : 4000;
   console.log(`Waiting ${ms}ms before restarting to prevent race case`);
   await delay(ms);
 });
 
-test("temp pass", t => {
-  t.pass();
+test(`http://localhost:4000 is reachable`, async t => {
+  t.true(await isReachable("http://localhost:4000"));
 });
-// test(`http://localhost:4000 is reachable`, async t => {
-//   t.true(await isReachable("http://localhost:4000"));
-// });
 
-// test(`http://localhost:4466 is reachable`, async t => {
-//   t.true(await isReachable("http://localhost:4466"));
-// });
+test(`http://localhost:4466 is reachable`, async t => {
+  t.true(await isReachable("http://localhost:4466"));
+});
 
 // test(`localhost:4000 is reachable`, async t => {
 //   t.true(await isReachable("localhost:4000"));
