2933fe1724d44dea1c164083f5bc9f199b2049ab
alechp <me@alechp.com>
2018-10-03 13:31:05 -0700

    Generated prisma client & created unit tests

--- src/service/tests/prisma.test.js
+++ src/service/tests/prisma.test.js
@@ -1,8 +1,12 @@
+require("dotenv").config();
 import test from "ava";
 import isReachable from "is-reachable";
 import delay from "delay";
+import moment from "moment";
 const isTravis = require("is-travis");
-const { prisma } = require("../../prisma-client");
+const { prisma } = require("../../../prisma-client");
+import { request } from "graphql-request";
+
 test.before(async t => {
   //TODO: Figure out less lazy solution; https://github.com/servexyz/prisma-starter/pull/25#issuecomment-419687114
   //? This maybe: https://github.com/servexyz/prisma-starter/issues/38
@@ -27,4 +31,34 @@ test(`await prisma.foos() via prisma-client`, async t => {
   t.truthy(foos);
 });
 
-test(`createSampleJson `);
+test("createSampleJson via graphql-request", async t => {
+  const mutation = `mutation createSampleJson($data:SampleJsonCreateInput!) {
+    createSampleJson(data:$data) {
+      id
+      json
+      jsons
+    }
+  }
+  `;
+
+  let time = moment()
+    .format("LTS")
+    .replace(/ /g, "");
+
+  const variables = {
+    data: {
+      json: { a: 1, time },
+      jsons: { set: [{ b: 2, c: 3 }, { d: 4, e: 5 }] }
+    }
+  };
+
+  t.notThrows(() => {
+    request("http://localhost:4000", mutation, variables)
+      .then(({ createSampleJson }) => {
+        console.log(`createSampleJson: ${createSampleJson}`);
+      })
+      .catch(err => {
+        console.log(`err: ${err}`);
+      });
+  });
+});
